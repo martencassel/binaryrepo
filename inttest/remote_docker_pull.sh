afe27cb99b5231ed892505301c50a4e2a0e353d0
martencassel <marten.cassel@gmail.com>
2022-01-13 17:45:53 +0100

    update integration test script

--- inttest/remote_docker_pull.sh
+++ inttest/remote_docker_pull.sh
@@ -6,21 +6,26 @@ unset https_proxy
 
 # Pull images through the remote proxy repo
 
+# Clear the local docker cache.
 docker rmi -f docker-remote.example.com/redis:latest
 docker rmi -f docker-remote.example.com/alpine:latest
 docker rmi -f docker-remote.example.com/postgres:latest
 
-docker pull docker-remote.example.com/redis:latest
-docker pull docker-remote.example.com/alpine:latest
-docker pull docker-remote.example.com/postgres:latest
+time docker pull docker-remote.example.com/redis:latest
+time docker pull docker-remote.example.com/alpine:latest
+time docker pull docker-remote.example.com/postgres:latest
 
+# Now the binaryrepo filestore is populated with image layers.
+
+# Clear the local docker cache once again.
 docker rmi -f docker-remote.example.com/redis:latest
 docker rmi -f docker-remote.example.com/alpine:latest
 docker rmi -f docker-remote.example.com/postgres:latest
 
-docker pull docker-remote.example.com/redis:latest
-docker pull docker-remote.example.com/alpine:latest
-docker pull docker-remote.example.com/postgres:latest
+# Now we will pull layers from binaryrepo's filestore cache.
+time docker pull docker-remote.example.com/redis:latest
+time docker pull docker-remote.example.com/alpine:latest
+time docker pull docker-remote.example.com/postgres:latest
 
 # Here goes the cache
 tree /tmp/filestore
