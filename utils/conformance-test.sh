420330ac95249b536114e59f1bf81e5c6883a458
martencassel <marten.cassel@gmail.com>
2022-01-25 19:17:17 +0100

    update conformance test

--- utils/conformance-test.sh
+++ utils/conformance-test.sh
@@ -9,5 +9,8 @@ export OCI_TEST_CONTENT_DISCOVERY=0
 export OCI_TEST_CONTENT_MANAGEMENT=0
 export OCI_DEBUG=true
 export OCI_NAMESPACE=alpine
-./conformance.test
+export OCI_BLOB_DIGEST="sha256:c059bfaa849c4d8e4aecaeb3a10c2d9b3d85f5165c66ad3a4d937758128c4d18"
+export OCI_MANIFEST_DIGEST="sha256:c059bfaa849c4d8e4aecaeb3a10c2d9b3d85f5165c66ad3a4d937758128c4d18"
+export OCI_TAG_NAME="latest"
+/tmp/distribution-spec/conformance/conformance.test
 
